188af9202556a595408f33c61189fe2a24366642
deniskyashif <denis.kyashif@gmail.com>
2018-01-15 23:04:38 +0200

    added some basic classification rules regarding the wh-word in the question

--- src/question_classifier.py
+++ src/question_classifier.py
@@ -14,15 +14,27 @@ class QuestionClass(Enum):
 
 
 # Reference: https://github.com/kimduho/nlp/wiki/Part-of-Speech-tags
-WH_PHRASE_LABELS = ["WHADJP", "WHAVP", "WHNP", "WHPP"]
+WH_PHRASE_LABELS = ["WHADJP", "WHADVP", "WHNP", "WHPP"]
 WH_WORD_LABELS = ["WDT", "WP", "WP$", "WRB"]
+
+WH_WORD_TO_TYPE_MAP = {
+    "what": QuestionClass.ENTY,
+    "when": QuestionClass.DESC,
+    "where": QuestionClass.LOC,
+    "which": QuestionClass.ENTY,
+    "who": QuestionClass.HUM,
+    "whom": QuestionClass.HUM,
+    "whose": QuestionClass.HUM,
+    "why": QuestionClass.DESC,
+    "how": QuestionClass.DESC
+}
 
 # Running the Stanford CoreNLP Server
 # https://stanfordnlp.github.io/CoreNLP/corenlp-server.html#getting-started
 NLP = StanfordCoreNLP('http://localhost:9000')
 
 
-def _find_in_parse_tree(node, predicate):
+def find_in_parse_tree(node, predicate):
     if not isinstance(node, Tree):
         return False
 
@@ -30,15 +42,18 @@ def _find_in_parse_tree(node, predicate):
         return node
 
     for child in node:
-        match = _find_in_parse_tree(child, predicate)
+        match = find_in_parse_tree(child, predicate)
         if match:
             return match
 
     return False
 
 
+def find_by_label(node, label):
+    return find_in_parse_tree(node, lambda x: x.label == label)
+
+
 def classify_question(question):
-    q_class = QuestionClass.NONE
     output = NLP.annotate(question, properties={
         "annotators": "parse",
         "outputFormat": "json"
@@ -47,16 +62,19 @@ def classify_question(question):
     tree = Tree.fromstring(output['sentences'][0]['parse'])
     tree.pretty_print()
 
-    wh_word_node = _find_in_parse_tree(
+    # TODO: Perform Head Noun Analysis
+
+    # Wh* Word Analysis
+    wh_word_node = find_in_parse_tree(
         tree, lambda x: x.label() in WH_WORD_LABELS)
 
     if wh_word_node:
-        wh_word = wh_word_node.leaves()[0]
-        print(wh_word)
-    else: print('no')
-    # TODO: Apply the classification rules
+        wh_word = (wh_word_node.leaves()[0]).lower()
+        # TODO: Cover more specific cases
+        return WH_WORD_TO_TYPE_MAP[wh_word]
 
-    return q_class
+    # TODO: Perform Main Verb Analysis
+    return QuestionClass.NONE
 
 
-classify_question("What is Dudley Do-Right's horse's name?")
+# print(classify_question("What is Dudley Do-Right's horse's name?"))
